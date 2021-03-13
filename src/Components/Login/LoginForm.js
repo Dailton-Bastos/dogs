b23064ccf185e6d400c4fafc4058422afa50eb9a
Dailton Bastos <dailtonbastos@gmail.com>
2021-03-13 16:15:05 -0300

    12 validate token

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -9,7 +9,7 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const { userLogin } = React.useContext(UserContext);
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -27,7 +27,15 @@ const LoginForm = () => {
 
         <Input label="Senha" type="password" name="password" {...password} />
 
-        <Button type="submit">Entrar</Button>
+        {loading ? (
+          <Button type="button" disabled>
+            Carregando...
+          </Button>
+        ) : (
+          <Button type="submit">Entrar</Button>
+        )}
+
+        {error && <p>{error}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
