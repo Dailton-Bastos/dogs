06170752b55f899fa540a9cf29cfe41bd606841e
Dailton Bastos <dailtonbastos@gmail.com>
2021-03-21 15:28:34 -0300

    23 feed modal 2

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -7,14 +7,14 @@ const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
 
   return (
-    <div className={styles.photos}>
+    <div className={styles.photo}>
       <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
       </div>
 
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
